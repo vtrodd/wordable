067a8383437eef261cc5fce93b62bfd2d8cf1ccd
vtrod <vic.trodd@gmail.com>
2022-01-18 22:49:30 +0100

    fixed token import from config

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,9 +1,9 @@
 import { REST } from '@discordjs/rest'
 import { Routes } from 'discord-api-types/rest/v9'
-import { CLIENT_ID, token } from './config.json'
+import { CLIENT_ID, TOKEN } from './config.json'
 import commands from './src/commands'
 
-const rest = new REST({ version: '9' }).setToken(token)
+const rest = new REST({ version: '9' }).setToken(TOKEN)
 
 console.log('🤖 Started refreshing application commands.')
 rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands.map(command => command.info.toJSON()) })
